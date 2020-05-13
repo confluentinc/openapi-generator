c2ce01233473aa8c092b12c9478116beeba88fc7
Arvind Thirunarayanan <arvth-git@yahoo.com>
2020-05-12 19:16:25 -0500

    Add new generateInterfaces option for go-experimental
      * Samples now contain interfaces, using backward incompatible method
        signatures.
      * Add one test for mocked methods

--- samples/client/petstore/go-experimental/pet_api_test.go
+++ samples/client/petstore/go-experimental/pet_api_test.go
@@ -3,12 +3,15 @@ package main
 import (
 	"context"
 	"fmt"
+	"net/http"
 	"os"
 	"testing"
 
+	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 
 	sw "./go-petstore"
+	apimock "./go-petstore/mock"
 )
 
 var client *sw.APIClient
@@ -40,6 +43,27 @@ func TestAddPet(t *testing.T) {
 	if r.StatusCode != 200 {
 		t.Log(r)
 	}
+}
+
+func TestAddPetMock(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockApi := apimock.NewMockPetApi(ctrl)
+
+	mockApi.EXPECT().
+		AddPetGetRequest(gomock.Any()).
+		Return(sw.ApiAddPetRequest{}).
+		MinTimes(1)	// ensure mock method is called at least once
+
+	mockApi.EXPECT().
+		AddPetExecute(gomock.Any()).
+		Return(&http.Response{StatusCode:200}, nil).
+		MinTimes(1)
+
+	actualApi := client.PetApi
+	client.PetApi = mockApi
+	TestAddPet(t)
+	client.PetApi = actualApi
 }
 
 func TestFindPetsByStatusWithMissingParam(t *testing.T) {
